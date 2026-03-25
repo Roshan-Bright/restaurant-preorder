eb13e19e2c0fc512152df5a91849fbe2385bddbd
Roshan-B-ctrl <roshanbright07@gmail.com>
2026-03-25 09:20:08 +0530

    fix bcrypt to bcryptjs for linux compatibility

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 // backend/controllers/authController.js
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt    = require('jsonwebtoken');
 const db     = require('../config/db');
 
